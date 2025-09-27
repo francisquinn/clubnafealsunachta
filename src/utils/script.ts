a87993694a24c9e2a6442943b7d71cfd9ea64519
francisquinn <francisquinnn@gmail.com>
2025-09-27 14:41:48 +0200

    remove past event and extract events.json to gist

--- src/utils/script.ts
+++ src/utils/script.ts
@@ -1,3 +1,5 @@
+const TOPICS_GIST = import.meta.env.TOPICS_GIST;
+
 export function formatBlogDate(date: Date): string {
   return `${date.toLocaleDateString("en-US", {
     month: "short",
@@ -12,4 +14,24 @@ export function formatEventDate(date: Date): string {
   })} ${date.getDate()} @ ${date.getHours()}:${String(
     date.getMinutes()
   ).padStart(2, "0")}`;
+}
+
+export async function fetchEventGistJson() {
+  try {
+    const gistRes = await fetch(TOPICS_GIST);
+    const data = await gistRes.json();
+    const file = data.files["cnf-topics.json"];
+
+    const rawRes = await fetch(file.raw_url);
+    return await rawRes.json();
+  } catch (e) {
+    console.error("whoops, an error has occured fetching the gist data :(", e);
+    return [];
+  }
+
+}
+
+export async function getCurrentEvent() {
+  const topics = await fetchEventGistJson();
+  return topics[0] ?? null;
 }
